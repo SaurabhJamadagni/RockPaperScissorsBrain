5a31f41d9d366314a4f81ec0d8419005af5fe7ba
Saurabh Jamadagni <saurabhjamadagni2002@gmail.com>
2022-07-21 14:38:35 +0530

    initial commit

---
+++ RockPaperScissorBrain/RockPaperScissorBrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorBrainApp.swift
+//  RockPaperScissorBrain
+//
+//  Created by Admin on 21/07/22.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorBrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
